7aa6b5adeb75d83ecdfe1169a6b943fc01a07f64
Malone Hedges <malonehedges@users.noreply.github.com>
2022-11-05 13:57:14 -0700

    hey

--- src/MeloVault.sol
+++ src/MeloVault.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.17;
 
 import "openzeppelin/token/ERC721/IERC721.sol";
+import "openzeppelin/token/ERC721/IERC721Receiver.sol";
+import "openzeppelin/token/ERC1155/IERC1155Receiver.sol";
 
 interface IVerifier {
     function verify(bytes calldata) external view returns (bool);
 }
 
-contract Honeydew {
+contract MeloVault is IERC1155Receiver, IERC721Receiver {
     struct Transaction {
         address to;
         uint256 value;
@@ -17,14 +19,16 @@ contract Honeydew {
 
     struct Proposal {
         uint256 endBlock;
+        string title;
+        string descriptionHash;
         Transaction[] transactions;
     }
     
-    event MeloVaultCreated(string indexed name);
+    event MeloVaultCreated(string indexed name, address indexed token);
 
     event ProposalCreated(
         bytes32 indexed id,
-        bytes32 snapshotBlockHash,
+        bytes32 indexed snapshotBlockHash,
         Proposal indexed proposal
     );
 
@@ -46,22 +50,22 @@ contract Honeydew {
         nft = IERC721(_nft);
         verifier = IVerifier(_verifier);
 
-        emit MeloVaultCreated(_name);
+        emit MeloVaultCreated(_name, _nft);
     }
 
     function propose(Proposal calldata proposal) external {
-        require(nft.balanceOf(msg.sender) > 0, "Honeydew: not a token holder");
+        require(nft.balanceOf(msg.sender) > 0, "MeloVault: not a token holder");
         require(
             proposal.endBlock > block.number,
-            "Honeydew: end block must be in the future"
+            "MeloVault: end block must be in the future"
         );
         require(
             proposal.endBlock <= block.number + maxBlocksInFuture,
-            "Honeydew: end block too far in the future"
+            "MeloVault: end block too far in the future"
         );
 
         bytes32 id = proposalHash(proposal);
-        require(proposals[id] == 0, "Honeydew: proposal already exists");
+        require(proposals[id] == 0, "MeloVault: proposal already exists");
 
         bytes32 snapshotBlockHash = blockhash(block.number - 1);
 
@@ -72,26 +76,31 @@ contract Honeydew {
     function executeProposal(Proposal calldata proposal, bytes calldata fact)
         external
     {
-        require(verifier.verify(fact), "Honeydew: invalid fact");
+        require(verifier.verify(fact), "MeloVault: invalid fact");
 
         bytes32 id = proposalHash(proposal);
-        require(proposals[id] != 0, "Honeydew: proposal does not exist");
-        require(block.number > proposal.endBlock, "Honeydew: too soon");
+        require(proposals[id] != 0, "MeloVault: proposal does not exist");
+        require(block.number > proposal.endBlock, "MeloVault: too soon");
         require(
             block.number <= proposal.endBlock + blocksAllowedForExecution,
-            "Honeydew: too late"
+            "MeloVault: too late"
         );
 
-        require(!executed[id], "Honeydew: already executed");
+        require(!executed[id], "MeloVault: already executed");
         executed[id] = true;
 
-        for (uint256 i = 0; i < proposal.transactions.length; i++) {
+        uint256 len = proposal.transactions.length;
+        for (uint256 i; i < len; ) {
             Transaction memory transaction = proposal.transactions[i];
             (bool success, ) = transaction.to.call{
                 value: transaction.value,
                 gas: transaction.gas
             }(transaction.data);
-            require(success, "Honeydew: transaction failed");
+            require(success, "MeloVault: transaction failed");
+
+            unchecked {
+                ++i;
+            }
         }
 
         emit ProposalExecuted(id, proposal);
@@ -108,11 +117,55 @@ contract Honeydew {
     /// admin ///
 
     modifier onlySelf() {
-        require(msg.sender == address(this), "Honeydew: not self");
+        require(msg.sender == address(this), "MeloVault: not self");
         _;
     }
 
     function setVerifier(address _verifier) external onlySelf {
         verifier = IVerifier(_verifier);
+    }
+
+    /// receive ///
+
+    receive() external payable {}
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata 
+    ) external pure returns (bytes4){
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        override
+        external
+        pure
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            interfaceId == type(IERC721Receiver).interfaceId;
     }
 }
--- test/MeloVault.t.sol
+++ test/MeloVault.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/Honeydew.sol";
+import "../src/MeloVault.sol";
 import "openzeppelin/token/ERC721/ERC721.sol";
 
 contract TestVerifier is IVerifier {
@@ -25,43 +25,63 @@ contract TestNFT is ERC721 {
     }
 }
 
-contract HoneydewTest is Test {
+contract MeloVaultTest is Test {
     
     TestNFT token;
     IVerifier verifier;
-    Honeydew honeydew;
+    MeloVault honeydew;
 
     event ProposalCreated(
         bytes32 indexed id,
-        bytes32 snapshotBlockHash,
-        Honeydew.Proposal indexed proposal
+        bytes32 indexed snapshotBlockHash,
+        MeloVault.Proposal indexed proposal
     );
 
-    event ProposalExecuted(bytes32 indexed id, Honeydew.Proposal indexed proposal);
+    event ProposalExecuted(bytes32 indexed id, MeloVault.Proposal indexed proposal);
 
     function setUp() public {
         token = new TestNFT();
         verifier = new TestVerifier();
-        honeydew = new Honeydew("MV", address(token), address(verifier));
+        honeydew = new MeloVault("MV", address(token), address(verifier));
     }
     
     function testDeploy() public {
-       new Honeydew("MV", address(0), address(0));
+       new MeloVault("MV", address(0), address(0));
     }
 
     function testProposeAndExecute() public {
-        address proposer = address(0x1);
-        
-        Honeydew.Proposal memory proposal = Honeydew.Proposal({
+        address proposer = address(1);
+
+        // ensure the multisig can receive ether and tokens
+        vm.deal(address(proposer), 1 ether);
+        vm.prank(proposer);
+        payable(address(honeydew)).transfer(1 ether);
+
+        token.mint(address(proposer), 2);
+        vm.prank(proposer);
+        token.safeTransferFrom(address(proposer), address(honeydew), 2);
+
+        // make a proposal with 1 transaction that sends 1 wei to the address(2)
+
+        MeloVault.Proposal memory proposal = MeloVault.Proposal({
             endBlock: block.number + 100,
-            transactions: new Honeydew.Transaction[](0)
+            title: "test",
+            descriptionHash: "test",
+            transactions: new MeloVault.Transaction[](1)
         });
+        proposal.transactions[0] = MeloVault.Transaction({
+            to: address(2),
+            value: 1,
+            data: new bytes(0),
+            gas: 1
+        });
+        
         bytes32 propId = honeydew.proposalHash(proposal);
 
         // propose
 
         vm.prank(proposer);
-        vm.expectRevert("Honeydew: not a token holder");
+        vm.expectRevert("MeloVault: not a token holder");
         honeydew.propose(proposal);
 
         token.mint(proposer, 1);
@@ -75,15 +95,15 @@ contract HoneydewTest is Test {
 
         bytes memory fact = abi.encode(0);
 
-        vm.expectRevert("Honeydew: too soon");
+        vm.expectRevert("MeloVault: too soon");
         honeydew.executeProposal(proposal, fact);
 
         vm.roll(proposal.endBlock);
-        vm.expectRevert("Honeydew: too soon");
+        vm.expectRevert("MeloVault: too soon");
         honeydew.executeProposal(proposal, fact);
 
         vm.roll(proposal.endBlock + honeydew.blocksAllowedForExecution() + 1);
-        vm.expectRevert("Honeydew: too late");
+        vm.expectRevert("MeloVault: too late");
         honeydew.executeProposal(proposal, fact);
 
         vm.roll(proposal.endBlock + 1);
@@ -91,7 +111,9 @@ contract HoneydewTest is Test {
         emit ProposalExecuted(propId, proposal);
         honeydew.executeProposal(proposal, fact);
 
-        vm.expectRevert("Honeydew: already executed");
+        assertEq(address(2).balance, 1);
+
+        vm.expectRevert("MeloVault: already executed");
         honeydew.executeProposal(proposal, fact);
     }
 }
