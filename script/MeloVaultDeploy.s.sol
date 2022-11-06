cc9c279fba29e4acf751ba023cb01ba443497e92
Malone Hedges <malonehedges@users.noreply.github.com>
2022-11-05 18:02:56 -0700

    use goerli verifier

--- script/MeloVaultDeploy.s.sol
+++ script/MeloVaultDeploy.s.sol
@@ -9,8 +9,9 @@ import "../src/SuspiciousStrawberry.sol";
 
 contract MeloVaultDeploy is Script {
     function run() public {
-        vm.broadcast();
-        IVerifier verifier = new TrueVerifier();
+        // vm.broadcast();
+        // IVerifier verifier = new TrueVerifier();
+        address verifierAddress = address(0xD6b9E04598f87b154a4797465c194C27341D5031);
         
         vm.broadcast();
         SuspiciousStrawberry strawberry = new SuspiciousStrawberry("base uri");
@@ -19,6 +20,6 @@ contract MeloVaultDeploy is Script {
         strawberry.mint(tx.origin, 1);
 
         vm.broadcast();
-        new MeloVault("my vault", address(strawberry), address(verifier));
+        new MeloVault("my vault", address(strawberry), verifierAddress);
     }
 }
